5a4c4dbaed4458e799efec69a2683c385d807780
andresfegome <andresfegome15@gmail.com>
2022-08-15 15:54:21 -0500

    delete row sugerencia

--- controller/contacto.controller.js
+++ controller/contacto.controller.js
@@ -8,13 +8,13 @@ const { catchAsync } = require("../utils/catchAsync.utils");
 const { Email } = require("../utils/email.utils");
 
 const getsugerencias = catchAsync(async (req, res, next) => {
-  const sugerencias = await Contacto.findAll();
-  res.status(202).json({ sugerencias });
+  const contactos = await Contacto.findAll();
+  res.status(202).json({ contactos });
 });
 
 const createContacto = catchAsync(async (req, res, next) => {
-  const { name, email, mensaje, sugerencia } = req.body;
-  await Contacto.create({ name, email, mensaje, sugerencia });
+  const { name, email, mensaje } = req.body;
+  await Contacto.create({ name, email, mensaje });
   await new Email(email).sendWelcome(name);
 
   res.status(201).json({ status: "success" });
